6667ea3a3a76d17bd2905d6b16a18a82d74a63b9
Vincenzo Bonelli <bv.vincenzo@gmail.com>
2021-03-05 00:28:35 -0400

    Primeros estilos y primera presentacion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
-import logoDP from "./images/logoDP.png";
-
 import "./styles/main.scss";
+import logoDP from "./images/logoDP.png";
+import appDesign from "./images/app-design.png";
+import backEnd from "./images/back-end.png";
+import cms from "./images/cms.png";
+import frontEnd from "./images/front-end.png";
+import webDev from "./images/web-dev.png";
+import webServ from "./images/web-service.png";
+
+var images = [webDev, frontEnd, backEnd, appDesign, cms, webServ];
+
+var imgElements = images.map(function (img) {
+  const imgElem = document.createElement("IMG");
+  imgElem.setAttribute("src", img);
+  imgElem.setAttribute("class", "img-res");
+  return imgElem;
+});
+
+const imgContainers = document.querySelectorAll(".habilidad-img");
+
+imgContainers.forEach((cont, i) => {
+  cont.appendChild(imgElements[i]);
+});
+
+var testImg = document.createElement("IMG");
+testImg.setAttribute("src", appDesign);
 
 console.log("Hello you!");
 
 var newImg = document.createElement("IMG");
 newImg.setAttribute("src", logoDP);
 newImg.setAttribute("alt", "Dulce Sin Pena");
-
-document.body.appendChild(newImg);
